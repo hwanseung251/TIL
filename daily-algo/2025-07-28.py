768e691de3ded249b67e15ae8aaf8513a930c97a
jhs <jhsjhs8566@gmail.com>
2025-08-29 21:17:43 +0900

    0829 commit

--- daily-algo/2025-07-28.py
+++
@@ -1,8 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-pipe = [[0] * (N+1) for _ in range(N+1)]
-for i in range(1,N+1):
-    pipe[i] = [0] + list(map(int, input().split()))
-print(pipe)
