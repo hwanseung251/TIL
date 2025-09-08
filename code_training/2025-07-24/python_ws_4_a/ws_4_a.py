70a5b8fe3bcdebd45b3232f67f3ec1389772f18c
jhs <jhsjhs8566@gmail.com>
2025-09-08 15:10:35 +0900

    update TIL

--- code_training/2025-07-24/python_ws_4_a/ws_4_a.py
+++
@@ -1,9 +0,0 @@
-# 아래에 코드를 작성하시오.
-from utils.create_url import create_url
-from conf.settings import NAME, MAIN_URL
-# import utils
-
-
-result = create_url(NAME, MAIN_URL)
-print(result)
-
--- code_training/2025-07-28/python_hw_5_2/hw_5_2.py
+++
@@ -1,7 +0,0 @@
-# 아래 함수를 수정하시오.
-def count_character(string, target):
-    return string.count(target)
-
-
-result = count_character("Hello, World!", "o")
-print(result)  # 2
